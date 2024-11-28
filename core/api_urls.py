c579e7d10aa6f0cb3fd12a41f8f03412242bdb49
Ahmad Saeed <ahmad5045841@gmail.com>
2024-11-28 12:23:11 +0500

    Added APIs

---
+++ core/api_urls.py
@@ -0,0 +1,44 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # User API
+    path('users/', views.UserListCreateAPIView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', views.UserRetrieveUpdateDestroyAPIView.as_view(), name='user-detail'),
+
+    # Menu APIs
+    path('menus/', views.MenuListCreateAPIView.as_view(), name='menu-list-create'),
+    path('menus/<int:pk>/', views.MenuRetrieveUpdateDestroyAPIView.as_view(), name='menu-detail'),
+
+    # Menu Group APIs
+    path('menu_groups/', views.MenuGroupListCreateAPIView.as_view(), name='menu-group-list-create'),
+    path('menu_groups/<int:pk>/', views.MenuGroupRetrieveUpdateDestroyAPIView.as_view(), name='menu-group-detail'),
+
+    # Menu Item APIs
+    path('menu_items/', views.MenuItemListCreateAPIView.as_view(), name='menu-item-list-create'),
+    path('menu_items/<int:pk>/', views.MenuItemRetrieveUpdateDestroyAPIView.as_view(), name='menu-item-detail'),
+
+    # Reservation APIs
+    path('reservations/', views.ReservationListCreateAPIView.as_view(), name='reservation-list-create'),
+    path('reservations/<int:pk>/', views.ReservationRetrieveUpdateDestroyAPIView.as_view(), name='reservation-detail'),
+
+    # Order APIs
+    path('orders/', views.OrderListCreateAPIView.as_view(), name='order-list-create'),
+    path('orders/<int:pk>/', views.OrderRetrieveUpdateDestroyAPIView.as_view(), name='order-detail'),
+
+    # Table APIs
+    path('tables/', views.TableListCreateAPIView.as_view(), name='table-list-create'),
+    path('tables/<int:pk>/', views.TableRetrieveUpdateDestroyAPIView.as_view(), name='table-detail'),
+
+    # Unregistered Order APIs
+    path('unregistered_orders/', views.UnregisteredOrderListCreateAPIView.as_view(), name='unregistered-order-list-create'),
+    path('unregistered_orders/<int:pk>/', views.UnregisteredOrderRetrieveUpdateDestroyAPIView.as_view(), name='unregistered-order-detail'),
+]
+
+# For token authentication:
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
---
+++ core/serializers.py
@@ -0,0 +1,100 @@
+from rest_framework import serializers
+'''
+from .models import User, Menu, MenuItem, Reservation, Order
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role']
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'name', 'description']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'menu', 'name', 'price', 'description', 'available']
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ['id', 'user', 'date', 'time', 'guests', 'status']
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'items', 'total_price', 'status']
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'role']  # Include fields relevant to your model
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            role=validated_data['role']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+'''
+
+
+#main Serializers
+from rest_framework import serializers
+from .models import User, Menu, MenuGroup, MenuItem, Reservation, Order, Table, UnregisteredOrder
+
+# User serializer
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'name', 'email', 'role', 'phone_number']
+
+# Menu serializer
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'name', 'description', 'image', 'is_active', 'created_at', 'updated_at']
+
+# Menu Group serializer
+class MenuGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuGroup
+        fields = ['id', 'menu', 'name', 'description', 'parent_group', 'is_active', 'created_at', 'updated_at']
+
+# Menu Item serializer
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'menugroup', 'name', 'price', 'description', 'image', 'available', 'created_at', 'updated_at']
+
+# Reservation serializer
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = ['id', 'user', 'date', 'time', 'guests', 'status']
+
+# Order serializer
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'menu_item', 'quantity', 'status', 'created_at']
+
+# Table serializer
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = ['id', 'table_number', 'is_booked']
+
+# Unregistered Order serializer
+class UnregisteredOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UnregisteredOrder
+        fields = ['id', 'menu_item', 'quantity', 'customer_name', 'table', 'order_time', 'status']
